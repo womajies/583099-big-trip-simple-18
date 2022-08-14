97725667ad5a1a9bae5a7bbf8778ddae9991f134
Шамиль Магомедов <shomainkognito@yandex.ru>
2022-08-14 16:35:36 +0300

    #fix исправил отрисовку сортировки и точек маршрутов

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -1,4 +1,3 @@
-import TripView from '../view/trip-view.js';
 import SortView from '../view/sort-view.js';
 import PointListView from '../view/point-list-view.js';
 import PointView from '../view/point-view.js';
@@ -6,15 +5,13 @@ import PointEditView from '../view/point-edit-view.js';
 import {render} from '../render.js';
 
 export default class TripPresenter {
-  tripComponent = new TripView();
   pointListComponent = new PointListView();
 
   init = (tripContainer) => {
     this.tripContainer = tripContainer;
 
-    render(this.tripComponent, this.tripContainer);
-    render(new SortView(), this.tripComponent.getElement());
-    render(this.pointListComponent, this.tripComponent.getElement());
+    render(new SortView(), this.tripContainer);
+    render(this.pointListComponent, this.tripContainer);
     render(new PointEditView(), this.pointListComponent.getElement());
 
     for (let i = 0; i < 3; i++) {
