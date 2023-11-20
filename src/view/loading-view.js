e5c40b0db5ec18ad75a956a5d8cf7c91e11299a0
Shamil Magomedov <shomainkognito@yandex.ru>
2023-11-20 15:30:17 +0300

    #fix build version for gh-pages

--- src/view/loading-view.js
+++
@@ -1,9 +0,0 @@
-import AbstractView from '../framework/view/abstract-view.js';
-
-const createLoadingTemplate = () => '<p class="trip-events__msg">Loading...</p>';
-
-export default class LoadingView extends AbstractView {
-  get template() {
-    return createLoadingTemplate();
-  }
-}
--- src/view/point-list-view.js
+++
@@ -1,9 +0,0 @@
-import AbstractView from '../framework/view/abstract-view.js';
-
-const createPointListViewTemplate = () => '<ul class="trip-events__list"></ul>';
-
-export default class PointListView extends AbstractView {
-  get template() {
-    return createPointListViewTemplate();
-  }
-}
